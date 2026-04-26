f49846e1852dd96a8db72186a2fbe5fdda693848
Will <willmiller629@gmail.com>
2026-04-26 14:35:00 -0400

    fahr to celsius

--- kr-ch1/ex_1_3.c
+++ kr-ch1/ex_1_3.c
@@ -2,6 +2,19 @@
 
 int main(void)
 {
-	printf("Hello");
+	float fahr, celsius;
+	int lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	printf("%3s %6s\n\n", "Farh", "Celsius");
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+	
 	return 0;
 }
