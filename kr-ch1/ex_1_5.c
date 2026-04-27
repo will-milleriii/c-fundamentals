09cf32553e1a3d810f40c280689007a8678289e2
Will <willmiller629@gmail.com>
2026-04-27 11:18:43 -0400

    temp conv for loop

---
+++ kr-ch1/ex_1_5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void tempConversion(void);
+
+int main(void)
+{
+	tempConversion();
+	return 0;
+}
+
+void tempConversion(void)
+{
+	int fahr;
+
+	for (fahr = 300; fahr >=0; fahr = fahr - 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
